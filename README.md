fac65598aaa115a4508a2a8eadcc80c521458bcc
Ryan McNair <ryan.mcnair@gmail.com>
2020-08-05 12:02:14 -0500

    created files for webdev, art and health and updated project cards

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ https://ryanmcnair-personal.netlify.app/
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/b2bf4fd0-a608-4d1d-9f12-1eca031e9100/deploy-status)](https://app.netlify.com/sites/ryanmcnair-personal/deploys)
 
-#E27D60
+
--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const projects = [
     {
     title: "Cool Project", 
     screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQIm5WcdPN0JE9ssXr0S0HuLu2QHO30ckmDAg&usqp=CAU", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    description: "This is the best project",
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
-    url: "https://github.com/ryanmcnair", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    url: "https://github.com/ryanmcnair",
     githubUrl: "https://github.com/ryanmcnair",
   }
 ];
@@ -15,23 +15,40 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
     }
     
-
 const createProjectCards = () => {
-    let domString = '';
-
-    for (let i = 0; i < projects.length; i++) {
-        domString += `<div class="project">`
-        domString +=    `<h1>${projects[i].title}</h1>`;
-        domString +=    `<div><img src=${projects[i].screenshot}></div>`;
-        domString +=    `<h2>Description: ${projects[i].description}</h2>`;
-        domString +=    `<h3>Technologies Used: ${projects[i].technologiesUsed}</h3>`;
-        domString +=    `<h3>${projects[i].available}</h3>`;
-        domString +=    `<h4><a href=${projects[i].url}>See the project here</a</h4>`;
-        domString +=    `<h5><a href="${projects[i].githubUrl}">GitHub</a></h5>`;
-        domString += `</div>`
- 
-    }
-    printToDom('projectsPage', domString);
+  let domString = "";
+    for(let i = 0; i < projects.length; i++){
+      if (projects[i].available){
+        domString+=`<div class="project-card">
+                      <img src="${projects[i].screenshot}" alt="${projects[i].title}" class="project-image">
+                      <div class="project-details">
+                      <h1 class="project-details-title">${projects[i].title}</h1>
+                             <p class="project-details-description">${projects[i].description}</p>
+                             <p class="project-details-technologies">${projects[i].technologiesUsed}</p>
+                             <p class="project-details-technologies">${projects[i].githubUrl}</p>
+                             <p class="project-details-link">${projects[i].url}</p>
+                            </div>
+                          </div>` 
+          }
+      }
+      printToDom("projectsPage", domString);
   }
+// const createProjectCards = () => {
+//     let domString = '';
+
+//     for (let i = 0; i < projects.length; i++) {
+//         domString += `<div class="project">`
+//         domString +=    `<h1>${projects[i].title}</h1>`;
+//         domString +=    `<div><img src=${projects[i].screenshot}></div>`;
+//         domString +=    `<h2>Description: ${projects[i].description}</h2>`;
+//         domString +=    `<h3>Technologies Used: ${projects[i].technologiesUsed}</h3>`;
+//         domString +=    `<h3>${projects[i].available}</h3>`;
+//         domString +=    `<h4><a href=${projects[i].url}>See the project here</a</h4>`;
+//         domString +=    `<h5><a href="${projects[i].githubUrl}">GitHub</a></h5>`;
+//         domString += `</div>`
+ 
+//     }
+//     printToDom('projectsPage', domString);
+//   }
   createProjectCards();
   console.log(projects[0].description)
